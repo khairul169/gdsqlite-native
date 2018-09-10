086894c7d5c6f251ca49a2c935e24483fdcccc41
khairul169 <me@khairul.my.id>
2018-09-10 21:21:38 +0700

    Remake module

---
+++ src/gdsqlite.hpp
@@ -0,0 +1,59 @@
+#ifndef GDSQLITE_H
+#define GDSQLITE_H
+
+#include <Godot.hpp>
+#include <Reference.hpp>
+
+#include <File.hpp>
+
+// SQLite3
+#include <sqlite3.h>
+#include <spmemvfs.h>
+
+#ifndef NULL
+#define NULL 0
+#endif
+
+namespace godot {
+	class SQLite : public Reference {
+		GODOT_CLASS(SQLite, Reference)
+
+	private:
+		// sqlite handler
+		sqlite3 *db;
+
+		// vfs
+		spmemvfs_db_t p_db;
+		bool memory_read;
+
+	public:
+		enum {
+			RESULT_BOTH = 0,
+			RESULT_NUM,
+			RESULT_ASSOC
+		};
+
+		static void _register_methods();
+
+		// constructor
+		SQLite();
+		~SQLite();
+		void _init() {}
+
+		// methods
+		bool open(String path);
+		bool open_buffered(String name, PoolByteArray buffers, int64_t size);
+		void close();
+
+		bool query(String statement);
+		Array fetch_array(String statement);
+		Array fetch_assoc(String statement);
+
+	private:
+		sqlite3_stmt* prepare(const char* statement);
+		Array fetch_rows(String query, int result_type = RESULT_BOTH);
+		Dictionary parse_row(sqlite3_stmt *stmt, int result_type);
+		sqlite3* get_handler() { return (memory_read ? p_db.handle : db); }
+	};
+}
+#endif
